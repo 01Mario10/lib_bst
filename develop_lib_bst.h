e3f64236871b20bda23664de0b731caf5c1e8775
Nicola De Marchi <nicolademarchi77@gmail.com>
2024-12-09 17:52:25 +0100

    Update develop_lib_bst.h

--- develop_lib_bst.h
+++ develop_lib_bst.h
@@ -1,24 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
 class Node {
 private:
-    int data;        
-    int weight;      
-    Node* lchild;    
-    Node* rchild;    
+    int data;
+    int weight;
+    Node* lchild;
+    Node* rchild;
 
 public:
-    
     Node(int k, int w = 0) : data(k), weight(w), lchild(nullptr), rchild(nullptr) {}
 
-   
     Node* insertR(int k) {
         if (k == this->data) {
             return this;
         }
-
         if (k < this->data) {
             if (this->lchild == nullptr) {
                 this->lchild = new Node(k);
@@ -32,7 +28,6 @@ public:
                 this->rchild = this->rchild->insertR(k);
             }
         }
-
         return this;
     }
 
@@ -42,16 +37,13 @@ public:
             if (k == current->data) {
                 return current; 
             }
-
             if (k < current->data) {
-             
                 if (current->lchild == nullptr) {
                     current->lchild = new Node(k);
                     return this;
                 }
                 current = current->lchild;
             } else {
-
                 if (current->rchild == nullptr) {
                     current->rchild = new Node(k);
                     return this;
@@ -60,7 +52,6 @@ public:
             }
         }
     }
-
 
     bool searchI(int k) {
         Node* current = this;
@@ -99,4 +90,63 @@ public:
             this->rchild->inOrder();
         }
     }
+
+    void preOrder(Node* node) {
+        if (node == nullptr)
+            return;
+        cout << node->data << " ";
+        preOrder(node->lchild);
+        preOrder(node->rchild);
+    }
+
+    void postOrder(Node* node) {
+        if (node == nullptr)
+            return;
+        postOrder(node->lchild);
+        postOrder(node->rchild);
+        cout << node->data << " ";
+    }
+
+    Node* deleteNode(Node* node, int k) {
+        if (node == nullptr)
+            return node;
+
+        if (k < node->data)
+            node->lchild = deleteNode(node->lchild, k);
+        else if (k > node->data)
+            node->rchild = deleteNode(node->rchild, k);
+        else {
+            if (node->lchild == nullptr) {
+                Node* temp = node->rchild;
+                delete node;
+                return temp;
+            } else if (node->rchild == nullptr) {
+                Node* temp = node->lchild;
+                delete node;
+                return temp;
+            }
+            Node* temp = node->rchild;
+            while (temp->lchild != nullptr)
+                temp = temp->lchild;
+            node->data = temp->data;
+            node->rchild = deleteNode(node->rchild, temp->data);
+        }
+        return node;
+    }
 };
+
+bool isBst(Node* node) {
+    if (node == nullptr) {
+        return true;
+    }
+
+    if (node->lchild != nullptr && node->lchild->data >= node->data) {
+        return false;
+    }
+
+    if (node->rchild != nullptr && node->rchild->data <= node->data) {
+        return false;
+    }
+
+    return isBst(node->lchild) && isBst(node->rchild);
+}
